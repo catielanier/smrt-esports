e4d27f657a8c1a64714421600e8ba15b6fcc1f3c
Catie Lanier <hello@catielanier.ca>
2024-11-04 22:48:14 -0500

    add base styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,14 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
+    colors: {
+      background: '#121212',
+      text: '#d1d1d1',
+      primary: '#5a89b7',
+      accent: '#84b9d4',
+      secondary: '#2a4e7c',
+      highlight: '#e0e0e0',
+    },
     extend: {}
   },
 
