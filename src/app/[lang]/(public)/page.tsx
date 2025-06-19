6e1b7f822c56ffcabe3db057a2643441a866a66e
Catie Lanier <hello@catielanier.ca>
2025-06-19 16:54:41 -0400

    fix class declarations

--- src/app/[lang]/(public)/page.tsx
+++ src/app/[lang]/(public)/page.tsx
@@ -92,10 +92,10 @@ export default async function Home({
                   ))}
                   {event.sublinks.map(sublink => (
                     <React.Fragment key={sublink.linkText}>
-                      <p className=${`font-heading py-0 text-l text-left ${index % 2 === 0 ? 'text-secondary' : 'text-accent'}`}>
+                      <p className={`font-heading py-0 text-l text-left ${index % 2 === 0 ? 'text-secondary' : 'text-accent'}`}>
                         {sublink.text}
                       </p>
-                      <p className=${`font-heading py-2 text-l text-left underline ${index % 2 === 0 ? 'text-secondary' : 'text-accent'}`}>
+                      <p className={`font-heading py-2 text-l text-left underline ${index % 2 === 0 ? 'text-secondary' : 'text-accent'}`}>
                         <a className={index % 2 === 0 ? 'text-background' : ''} href={sublink.url}>
                           {sublink.linkText}
                         </a>
